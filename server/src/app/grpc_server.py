eaf92e0e4bf7a04b909b94188b20dd0a7d98b2be
Miguelecf <mxcf98@gmail.com>
2024-09-13 05:01:43 -0300

    Updated test placeholders to actual names across the project

--- server/src/app/grpc_server.py
+++ server/src/app/grpc_server.py
@@ -2,25 +2,25 @@
 import grpc
 from concurrent import futures
 from src.use_cases.create_user import CreateUser
-from src.generated import login_pb2
-from src.generated import login_pb2_grpc
+from src.generated import user_pb2
+from src.generated import user_pb2_grpc
 from use_cases.login_use_case import LoginUseCase
 from src.infrastucture.user_repository import MySQLUserRepository
 
-class Greeter(login_pb2_grpc.GreeterServicer):
+class UserService(user_pb2_grpc.UserServiceServicer):
     def __init__(self, login_use_case: LoginUseCase,create_user: CreateUser):
         self.login_use_case = login_use_case
         self.create_user = create_user
 
     def Login(self, request, context):
-        print(f"Recibida solicitud de login: usuario={request.user}")
-        if not self.login_use_case.login(request.user, request.password):
+        print(f"Recibida solicitud de login: usuario={request.username}")
+        if not self.login_use_case.login(request.username, request.password):
             print("Autenticación fallida")
             context.set_details("Login failed!")
             context.set_code(grpc.StatusCode.UNAUTHENTICATED)
-            return login_pb2.LoginResponse(message="Login failed!")
+            return user_pb2.LoginResponse(message="Login failed!")
         print("Autenticación exitosa")
-        return login_pb2.LoginResponse(message="Login successful")
+        return user_pb2.LoginResponse(message="Login successful")
     
     def CreateUser(self, request, context):
         print(f"Recibida solicitud de creación de usuario: usuario={request.username}")
@@ -29,17 +29,18 @@ class Greeter(login_pb2_grpc.GreeterServicer):
             print("Creación de usuario fallida")
             context.set_details("User creation failed!")
             context.set_code(grpc.StatusCode.INTERNAL)
-            return login_pb2.CreateUserResponse(message="User creation failed!")
+            return user_pb2.CreateUserResponse(message="User creation failed!")
         print("Usuario creado exitosamente")
-        return login_pb2.CreateUserResponse(message="User created successfully")
+        return user_pb2.CreateUserResponse(message="User created successfully")
     
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     login_use_case = LoginUseCase(MySQLUserRepository())
     create_user = CreateUser(MySQLUserRepository())
-    login_pb2_grpc.add_GreeterServicer_to_server(Greeter(login_use_case,create_user), server)
+    user_pb2_grpc.add_UserServiceServicer_to_server(UserService(login_use_case,create_user), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print("Servidor gRPC iniciado en el puerto 50051")
     server.wait_for_termination()
+
