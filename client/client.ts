21b99dd77ec4cb10e809287218af719900dee005
Miguelecf <mxcf98@gmail.com>
2024-09-26 15:12:39 -0300

    Last changes

--- client/client.ts
+++ client/client.ts
@@ -3,6 +3,7 @@ import * as protoLoader from "@grpc/proto-loader";
 import { rejects } from "assert";
 import * as path from "path";
 
+
 // Paths to both .proto files
 const userProtoPath = path.resolve(__dirname, "../proto/user.proto");
 const storeProtoPath = path.resolve(__dirname, "../proto/store.proto");
@@ -29,7 +30,8 @@ class Client {
         this.productClient = new productProto.product.ProductService(host, grpc.credentials.createInsecure());
     }
 
-    async createUser(username: string, password: string, firstName: string, lastName: string, enabled: boolean, storeId?: number): Promise<string> {
+    async createUser(username: string, password: string, firstName: string, lastName: string, 
+                    enabled: boolean, storeId?: number): Promise<string> {
         return new Promise((resolve, reject) => {
             this.userClient.CreateUser(
                 {
